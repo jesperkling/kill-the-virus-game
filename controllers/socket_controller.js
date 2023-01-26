34588d3d419d2404b8013a11ea234fbbaa4e2411
Jesper Kling <jesperkling87@gmail.com>
2023-01-26 11:45:23 -0600

    Fixed mistake that crashed server on disconnect

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -8,24 +8,9 @@ const sessions = [];
 const handleDisconnect = function() {
 	debug(`Client ${this.id} disconnected :(`);
 
-	this.broadcast("user:disconnected", users[this.id]);
+	this.broadcast.emit("user:disconnected", users[this.id]);
 
 	delete users[this.id];
-}
-
-const handleUserJoined = function(username, callback) {
-	users[this.id] = username;
-
-	const startGame = createSession(this, username);
-
-	debug(`User ${username} with socket id ${this.id} joined`);
-
-	this.broadcast.emit("user:connected", username);
-
-	callback({
-		success: true,
-		start: startGame,
-	})
 }
 
 const createSession = function(socket, username) {
@@ -62,25 +47,80 @@ const createSession = function(socket, username) {
 
 	debug(`User ${username} joined session ${sessionToJoin}`);
 
-	socket.broadcast.to(sessionToJoin).emit("user:session", username, sessionToJoin, startGame);
+	io.in(sessionToJoin).emit("user:session", username, sessionToJoin, startGame);
 
-	return startGame;
+	return {
+		start: startGame,
+		session: sessionToJoin,
+		player: socket.id,
+	}
 }
 
-const handleGame = function(callback) {
+const handleUserJoined = function(username, callback) {
+	users[this.id] = username;
+
+	const startGame = createSession(this, username);
+
+	debug(`User ${username} with socket id ${this.id} joined`);
+
+	this.broadcast.emit("user:connected", username);
+
+	callback({
+		success: true,
+		start: startGame.start,
+		session: startGame.session,
+		player: startGame.player,
+	})
+}
+
+let holder = null;
+
+const handleGame = function(session, player, callback) {
+
+	if (holder === null) {
+		holder = session
+		return
+	} else {
+		holder = null
+	}
+
 	const y = Math.floor(Math.random() * 10) + 1;
 	const x = Math.floor(Math.random() * 15) + 1;
 
 	const time = Math.floor(Math.random() * 4000) + 1;
 
-	callback({
+	const data = {
 		success: true,
+		session,
 		y: y,
 		x: x,
 		time: time,
-	})
+	}
+
+	io.in(session).emit("game:success", data);
 }
 
+let waiter = null;
+let compareReaction;
+let winner;
+
+const handleGamePoint = function(reactionTime, player, session) {
+	
+	if (waiter === null) {
+		waiter = true;
+		compareReaction = {
+			reactionTime,
+			player,
+		}
+		return;
+	} else {
+		holder = null;
+	}
+
+	compareReaction.reactionTime > reactionTime ? winner = player : winner = compareReaction.player;
+
+	io.in(session).emit("game:result", winner);
+}
 
 module.exports = function(socket, _io) {
 	io = _io;
@@ -90,5 +130,7 @@ module.exports = function(socket, _io) {
 	socket.on("user:joined", handleUserJoined);
 
 	socket.on("user:startgame", handleGame);
+
+	socket.on("game:point", handleGamePoint);
 	
 }
